87cdb12892a0989c8c60a0b1472ec369dc62ccce
Nico Trost <nico.trost@amd.com>
2018-11-27 16:27:59 +0100

    added hip functions and structure to MGData

--- src/MGData.hpp
+++ src/MGData.hpp
@@ -22,6 +22,9 @@
 #define MGDATA_HPP
 
 #include <cassert>
+#include <hip/hip_runtime_api.h>
+
+#include "utils.hpp"
 #include "SparseMatrix.hpp"
 #include "Vector.hpp"
 
@@ -37,6 +40,8 @@ struct MGData_STRUCT {
    used inside optimized ComputeSPMV().
    */
   void * optimizationData;
+
+  local_int_t* hip; //!< f2cOperator on device
 };
 typedef struct MGData_STRUCT MGData;
 
@@ -47,14 +52,19 @@ typedef struct MGData_STRUCT MGData;
  @param[in] f2cOperator -
  @param[out] data the data structure for CG vectors that will be allocated to get it ready for use in CG iterations
  */
-inline void InitializeMGData(local_int_t * f2cOperator, Vector * rc, Vector * xc, Vector * Axf, MGData & data) {
-  data.numberOfPresmootherSteps = 1;
-  data.numberOfPostsmootherSteps = 1;
+inline void InitializeMGData(local_int_t * f2cOperator, MGData & data) {
   data.f2cOperator = f2cOperator; // Space for injection operator
-  data.rc = rc;
-  data.xc = xc;
-  data.Axf = Axf;
   return;
+}
+
+inline void HIPInitializeMGData(local_int_t* f2cOperator, Vector* rc, Vector* xc, Vector* Axf, MGData& data)
+{
+    data.numberOfPresmootherSteps = 1;
+    data.numberOfPostsmootherSteps = 1;
+    data.hip = f2cOperator;
+    data.rc = rc;
+    data.xc = xc;
+    data.Axf = Axf;
 }
 
 /*!
@@ -68,9 +78,14 @@ inline void DeleteMGData(MGData & data) {
   DeleteVector(*data.Axf);
   DeleteVector(*data.rc);
   DeleteVector(*data.xc);
+  HIPDeleteVector(*data.rc);
+  HIPDeleteVector(*data.xc);
   delete data.Axf;
   delete data.rc;
   delete data.xc;
+
+  HIP_CHECK(hipFree(data.hip));
+
   return;
 }
 

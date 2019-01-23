f47362570eb6f25e8660cf38877aa9cb4504d0f9
Nico Trost <nico.trost@amd.com>
2019-01-23 14:15:10 +0100

    fixed issue with spmv where wrong block offset was computed added coarse matrix spmv for MG cycle

--- src/ComputeSPMV.cpp
+++ src/ComputeSPMV.cpp
@@ -23,6 +23,46 @@
 
 #include <hip/hip_runtime.h>
 
+__global__ void kernel_spmv_ell_coarse(local_int_t size,
+                                       local_int_t m,
+                                       local_int_t n,
+                                       local_int_t ell_width,
+                                       const local_int_t* ell_col_ind,
+                                       const double* ell_val,
+                                       const local_int_t* perm,
+                                       const local_int_t* f2cOperator,
+                                       const double* x,
+                                       double* y)
+{
+    local_int_t gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(gid >= size)
+    {
+        return;
+    }
+
+    local_int_t row = perm[f2cOperator[gid]];
+
+    double sum = 0.0;
+
+    for(local_int_t p = 0; p < ell_width; ++p)
+    {
+        local_int_t idx = p * m + row;
+        local_int_t col = ell_col_ind[idx];
+
+        if(col >= 0 && col < n)
+        {
+            sum = fma(ell_val[idx], x[col], sum);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    y[row] = sum;
+}
+
 __global__ void kernel_spmv_ell(local_int_t m,
                                 int nblocks,
                                 local_int_t blocksize,
@@ -32,8 +72,28 @@ __global__ void kernel_spmv_ell(local_int_t m,
                                 const double* x,
                                 double* y)
 {
-    local_int_t gid = blockIdx.x * blockDim.x + threadIdx.x;
-    local_int_t row = blockIdx.x / nblocks * blockDim.x + threadIdx.x + (gid & (nblocks * blockDim.x - 1)) / blockDim.x * blocksize;
+    // Each block processes "nblocks" chunks of "blocksize" such that x vector loads are
+    // identical over several wavefronts. Furthermore, "blocksize" has to be sufficiently
+    // large, such that there is no penalty from loading the matrix.
+
+    // ID of the current block
+    local_int_t block_id = hipThreadIdx_x / blocksize;
+
+    // Thread ID within the current block
+    local_int_t block_tid = hipThreadIdx_x & (blocksize - 1);
+
+    // Offset into x vector between different thread blocks
+    local_int_t offset = hipBlockIdx_x * blocksize;
+
+    // Thread ID local to the current block
+    local_int_t block_lid = block_tid + offset;
+
+    // Rows per block
+    local_int_t rows_per_block = m / nblocks;
+
+    // Current row can be computed by global offset into the block plus
+    // the local ID within the current block
+    local_int_t row = block_id * rows_per_block + block_lid;
 
     if(row >= m)
     {
@@ -160,60 +220,88 @@ int ComputeSPMV(const SparseMatrix& A, Vector& x, Vector& y)
     }
 #endif
 
-    hipLaunchKernelGGL((kernel_spmv_ell),
-                       dim3((A.localNumberOfRows - 1) / 128 + 1),
-                       dim3(128),
-                       0,
-                       stream_interior,
-                       A.localNumberOfRows,
-                       A.nblocks,
-                       A.localNumberOfRows / A.nblocks,
-                       A.ell_width,
-                       A.ell_col_ind,
-                       A.ell_val,
-                       x.d_values,
-                       y.d_values);
+    if(&y != A.mgData->Axf)
+    {
+#define ELLMV_DIM 1024
+        hipLaunchKernelGGL((kernel_spmv_ell),
+                           dim3((A.localNumberOfRows - 1) / ELLMV_DIM + 1),
+                           dim3(ELLMV_DIM),
+                           0,
+                           stream_interior,
+                           A.localNumberOfRows,
+                           A.nblocks,
+                           ELLMV_DIM / A.nblocks,
+                           A.ell_width,
+                           A.ell_col_ind,
+                           A.ell_val,
+                           x.d_values,
+                           y.d_values);
+#undef ELLMV_DIM
+    }
 
 #ifndef HPCG_NO_MPI
     if(A.geom->size > 1)
     {
         ExchangeHaloAsync(A);
         ObtainRecvBuffer(A, x);
+
+        if(&y != A.mgData->Axf)
+        {
 #if 1
-        hipLaunchKernelGGL((kernel_spmv_halo),
-                           dim3((A.halo_rows - 1) / 128 + 1),
-                           dim3(128),
-                           0,
-                           0,
-                           A.halo_rows,
-                           A.localNumberOfColumns,
-                           A.ell_width,
-                           A.halo_row_ind,
-                           A.halo_col_ind,
-                           A.halo_val,
-                           A.perm,
-                           x.d_values,
-                           y.d_values);
+            hipLaunchKernelGGL((kernel_spmv_halo),
+                               dim3((A.halo_rows - 1) / 128 + 1),
+                               dim3(128),
+                               0,
+                               0,
+                               A.halo_rows,
+                               A.localNumberOfColumns,
+                               A.ell_width,
+                               A.halo_row_ind,
+                               A.halo_col_ind,
+                               A.halo_val,
+                               A.perm,
+                               x.d_values,
+                               y.d_values);
 #else
-        hipLaunchKernelGGL((kernel_spmv_halo),
-                           dim3((A.halo_rows - 1) / 128 + 1),
+            hipLaunchKernelGGL((kernel_spmv_halo),
+                               dim3((A.halo_rows - 1) / 128 + 1),
+                               dim3(128),
+                               0,
+                               0,
+                               A.halo_rows,
+                               A.localNumberOfRows,
+                               A.localNumberOfColumns,
+                               A.ell_width,
+                               A.halo_row_ind,
+                               A.halo_offset,
+                               A.ell_col_ind,
+                               A.ell_val,
+                               A.perm,
+                               x.d_values,
+                               y.d_values);
+#endif
+        }
+    }
+#endif
+
+    if(&y == A.mgData->Axf)
+    {
+        hipLaunchKernelGGL((kernel_spmv_ell_coarse),
+                           dim3((A.mgData->rc->localLength - 1) / 128 + 1),
                            dim3(128),
                            0,
                            0,
-                           A.halo_rows,
+                           A.mgData->rc->localLength,
                            A.localNumberOfRows,
                            A.localNumberOfColumns,
                            A.ell_width,
-                           A.halo_row_ind,
-                           A.halo_offset,
                            A.ell_col_ind,
                            A.ell_val,
                            A.perm,
+                           A.mgData->d_f2cOperator,
                            x.d_values,
                            y.d_values);
-#endif
     }
-#endif
 
     return 0;
 }

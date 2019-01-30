bf02dc27da25e51e4d1332340b72dbc58aef32a1
Nico Trost <nico.trost@amd.com>
2019-01-30 12:16:38 +0100

    random seed updated

--- src/utils.hpp
+++ src/utils.hpp
@@ -35,7 +35,7 @@ extern hiprandGenerator_t rng;
 extern curandGenerator_t rng;
 #endif
 
-#define RNG_SEED 25871479ULL
+#define RNG_SEED 0x586744
 #define MAX_COLORS 128
 
 #define HIP_CHECK(err)                                              \

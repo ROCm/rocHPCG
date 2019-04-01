4475c10d8cada512e4922ee1b70c0ec5a86e6f27
Nico Trost <nico.trost@amd.com>
2019-04-01 14:05:13 +0200

    updated to hpcg 3.1

--- src/ReportResults.cpp
+++ src/ReportResults.cpp
@@ -24,9 +24,8 @@
 
 #include <vector>
 #include "ReportResults.hpp"
-//#include "OutputFile.hpp"
+#include "OutputFile.hpp"
 #include "OptimizeProblem.hpp"
-#include "YAML_Doc.hpp"
 
 #ifdef HPCG_DEBUG
 #include <fstream>
@@ -240,9 +239,8 @@ void ReportResults(const SparseMatrix & A, int numberOfMgLevels, int numberOfCgS
     double fnbytesPerEquation = fnbytes/fnrow;
 
     // Instantiate YAML document
-//    OutputFile doc("HPCG-Benchmark", "3.0");
-    YAML_Doc doc("HPCG-Benchmark", "3.0");
-    doc.add("Release date", "November 11, 2015");
+    OutputFile doc("HPCG-Benchmark", "3.1");
+    doc.add("Release date", "March 28, 2019");
 
     doc.add("Machine Summary","");
     doc.get("Machine Summary")->add("Distributed Processes",A.geom->size);
@@ -437,8 +435,7 @@ void ReportResults(const SparseMatrix & A, int numberOfMgLevels, int numberOfCgS
       doc.get("Final Summary")->add("Please review the YAML file contents","You may NOT submit these results for consideration.");
     }
 
-//    std::string yaml = doc.generate();
-    std::string yaml = doc.generateYAML();
+    std::string yaml = doc.generate();
 #ifdef HPCG_DEBUG
     HPCG_fout << yaml;
 #endif
@@ -488,6 +485,5 @@ void ReportResults(const SparseMatrix & A, int numberOfMgLevels, int numberOfCgS
            totalGflops / A.geom->size,
            (frefnreads + frefnwrites) / (times[0] + fNumberOfCgSets * (times[7] / 10.0 + times[9] / 10.0)) / 1e9 / A.geom->size);
   }
-
   return;
 }

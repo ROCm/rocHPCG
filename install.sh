512d68c3fd9dcb6e5bec46b5418369e94cc093e7
Nico Trost <nico.trost@amd.com>
2019-06-25 12:08:51 +0200

    added rocprim dependency to install script

--- install.sh
+++ install.sh
@@ -110,9 +110,9 @@ install_packages( )
   fi
 
   # dependencies needed for executable to build
-  local library_dependencies_ubuntu=( "make" "hip_hcc" "pkg-config" "libnuma1" "rocrand" "cmake" "libnuma-dev" )
-  local library_dependencies_centos=( "epel-release" "make" "cmake3" "hip_hcc" "gcc-c++" "rpm-build" "numactl-libs" "rocrand" )
-  local library_dependencies_fedora=( "make" "cmake" "hip_hcc" "gcc-c++" "libcxx-devel" "rpm-build" "numactl-libs" "rocrand" )
+  local library_dependencies_ubuntu=( "make" "hip_hcc" "pkg-config" "libnuma1" "rocrand" "rocprim" "cmake" "libnuma-dev" )
+  local library_dependencies_centos=( "epel-release" "make" "cmake3" "hip_hcc" "gcc-c++" "rpm-build" "numactl-libs" "rocrand" "rocprim" )
+  local library_dependencies_fedora=( "make" "cmake" "hip_hcc" "gcc-c++" "libcxx-devel" "rpm-build" "numactl-libs" "rocrand" "rocprim" )
 
   case "${ID}" in
     ubuntu)

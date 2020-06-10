838042af30d7dba9946e48e35e80f00446f248db
Nico Trost <nico.trost@amd.com>
2020-06-10 10:36:53 +0200

    cmake updated and version bump

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,4 +1,4 @@
-# Modifications (c) 2019 Advanced Micro Devices, Inc.
+# Modifications (c) 2019-2020 Advanced Micro Devices, Inc.
 #
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
@@ -29,7 +29,7 @@
 find_package(Git REQUIRED)
 
 # Add some paths
-list(APPEND CMAKE_PREFIX_PATH /opt/rocm/hcc /opt/rocm/hip /opt/rocm)
+list(APPEND CMAKE_PREFIX_PATH /opt/rocm/hip /opt/rocm)
 
 # Find OpenMP package
 find_package(OpenMP)

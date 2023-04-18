df3c41ce683f207e74e995e2c3d58ae8ca3948af
gregrodgers <gregory.rodgers@amd.com>
2023-04-18 17:56:50 -0400

    Implement ComputeDotProduct with OpenMP target offload

--- omptarget/hpcg/src/ComputeDotProduct.cpp
+++ omptarget/hpcg/src/ComputeDotProduct.cpp
@@ -18,8 +18,15 @@
  HPCG routine
  */
 
+#ifndef HPCG_NO_MPI
+#include <mpi.h>
+#include "mytimer.hpp"
+#endif
+#ifndef HPCG_NO_OPENMP
+#include <omp.h>
+#endif
+#include <cassert>
 #include "ComputeDotProduct.hpp"
-#include "ComputeDotProduct_ref.hpp"
 
 /*!
   Routine to compute the dot product of two vectors.
@@ -36,12 +43,49 @@
 
   @return returns 0 upon success and non-zero otherwise
 
-  @see ComputeDotProduct_ref
 */
 int ComputeDotProduct(const local_int_t n, const Vector & x, const Vector & y,
     double & result, double & time_allreduce, bool & isOptimized) {
 
-  // This line and the next two lines should be removed and your version of ComputeDotProduct should be used.
-  isOptimized = false;
-  return ComputeDotProduct_ref(n, x, y, result, time_allreduce);
+  isOptimized = true;
+  assert(x.localLength>=n); // Test vector lengths
+  assert(y.localLength>=n);
+
+  double local_result = 0.0;
+  double * xv = x.values;
+  double * yv = y.values;
+  if (yv==xv) {
+#ifndef HPCG_NO_OPENMP
+#ifdef HPCG_OPENMP_TARGET
+    #pragma omp target teams distribute parallel for reduction (+:local_result) map(tofrom:xv[0:n])
+#else
+    #pragma omp parallel for reduction (+:local_result)
+#endif
+#endif
+    for (local_int_t i=0; i<n; i++) local_result += xv[i]*xv[i];
+  } else {
+#ifndef HPCG_NO_OPENMP
+#ifdef HPCG_OPENMP_TARGET
+    #pragma omp target teams distribute parallel for reduction (+:local_result) map(tofrom:xv[0:n],yv[0:n])
+#else
+    #pragma omp parallel for reduction (+:local_result)
+#endif
+#endif
+    for (local_int_t i=0; i<n; i++) local_result += xv[i]*yv[i];
+  }
+
+#ifndef HPCG_NO_MPI
+  // Use MPI's reduce function to collect all partial sums
+  double t0 = mytimer();
+  double global_result = 0.0;
+  MPI_Allreduce(&local_result, &global_result, 1, MPI_DOUBLE, MPI_SUM,
+      MPI_COMM_WORLD);
+  result = global_result;
+  time_allreduce += mytimer() - t0;
+#else
+  time_allreduce += 0.0;
+  result = local_result;
+#endif
+
+  return 0;
 }

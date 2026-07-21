c0b127cb5d39b299cc6fb712254d2671bb3cef9d
Nico <31079890+ntrost57@users.noreply.github.com>
2026-07-21 13:19:19 +0200

    merge in latest hpcg changes (#101)
    * update to latest hpcg changes
    
    * exit flag

--- src/CG_ref.cpp
+++ src/CG_ref.cpp
@@ -126,8 +126,8 @@ int CG_ref(const SparseMatrix & A, CGData & data, const Vector & b, Vector & x,
   normr0 = normr;
 
   // Start iterations
-
-  for (int k=1; k<=max_iter && normr/normr0 > tolerance; k++ ) {
+  // Convergence check accepts an error of no more than 6 significant digits of tolerance
+  for (int k=1; k<=max_iter && normr/normr0 > tolerance * (1.0 + 1.0e-6); k++ ) {
     TICK();
     if (doPreconditioning)
       ComputeMG_ref(A, r, z); // Apply preconditioner
